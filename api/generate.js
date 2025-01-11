52d0159de50d495ce49a8527b6a85cd38549dff7
GBNinon <safiraninon@gmail.com>
2025-01-12 00:58:17 +0100

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -57,9 +57,4 @@ app.post('/api/generate', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-
 module.exports = app;
