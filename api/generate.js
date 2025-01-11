be23448606d820ee18741af212f9e965e5c86c85
GBNinon <safiraninon@gmail.com>
2025-01-12 00:25:18 +0100

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -14,7 +14,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
-app.use(bodyParser.json());
+app.use(cors({ origin: '*' }));
 
 app.post('/api/generate', async (req, res) => {
     const { prompt } = req.body;
